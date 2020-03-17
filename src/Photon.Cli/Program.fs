2a9ba467a766e55e330f81864b562d0656d63a54
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2020-03-17 20:59:31 +0100

    init version

---
+++ src/Photon.Cli/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let path = System.IO.Path.Combine("src", "Photon.Cli", "bin", "Debug", "netcoreapp3.1", "Photon.dll")
+    let (modules, types) =  Photon.Core.getDocs path
+    printfn "MODULES: %A" modules
+    printfn "TYPES: %A" types
+    0 // return an integer exit code
---
+++ src/Photon/Library.fs
@@ -0,0 +1,74 @@
+﻿namespace Photon
+
+module Core =
+    open FSharp.Formatting
+    open FSharp.MetadataFormat
+
+    type DocInfo<'a> = {
+        ParentName: string
+        NamespaceName: string
+        Info: 'a
+    }
+
+
+
+
+
+    let getDocs (fileName: string) =
+        let props = ["project-name", "Photon"]
+
+        let output = MetadataFormat.Generate(fileName, parameters = props, markDownComments = false, publicOnly = true)
+
+        let allModules =
+            let rec collectModules pn nn (m: Module) =
+                [
+                    yield { ParentName = pn; NamespaceName = nn; Info =  m}
+                    yield! m.NestedModules |> List.collect (collectModules m.Name nn )
+                ]
+            output.AssemblyGroup.Namespaces
+            |> List.collect (fun n ->
+                n.Modules |> List.collect (fun m -> collectModules n.Name n.Name m)
+            )
+
+        let allTypes =
+            [
+                yield!
+                    output.AssemblyGroup.Namespaces
+                    |> List.collect (fun n ->
+                        n.Types |> List.map (fun t -> {ParentName = n.Name; NamespaceName = n.Name; Info = t} )
+                    )
+                yield!
+                    allModules
+                    |> List.collect (fun n ->
+                        n.Info.NestedTypes |> List.map (fun t -> {ParentName = n.Info.Name;  NamespaceName = n.NamespaceName; Info = t}) )
+            ]
+
+        allModules, allTypes
+
+
+
+///Some comment on the module
+module Say =
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    /// <example>
+    /// <code>
+    /// let v = hello "Chris"
+    /// v = "Hello Chris"
+    /// </code>
+    /// </example>
+    let hello name =
+        printfn "Hello %s" name
+
+    ///Some comment on the type
+    type SampleType = {
+        ///Some more comments
+        A: string
+        ///And more comments
+        B: int
+
+    }
